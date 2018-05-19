f31a7a7a93bd12ff42683241e10013dae07c8dd5
Daniela Bourges <daniela@addgene.org>
2018-05-18 22:32:43 -0400

    Fixed name of script to call

--- setup/setup-research-workstation.sh
+++ setup/setup-research-workstation.sh
@@ -8,9 +8,10 @@ echo -e "\n** Creating / updating global prerequisites...\n"
 $script_dir/1-setup-usr-local.sh $1
 
 echo -e "\n** Creating / updating Bioinformatics virtual environment...\n"
-$script_dir/2-setup-prodops-virtualenv.sh $1
+$script_dir/2-setup-bioinformatics-virtualenv.sh $1
 
 echo -e "\n---\n"
-echo -e "All pre-requisites are setup OK!\n"
-echo -e "Now close all of your terminal windows and open a fresh one (so everything is loaded cleanly)\n"
-echo -e "After you have done that, navigate back to the research repo directory and type: workon bioinformatics
+echo -e "Your environment was correctly set up!\n"
+echo -e "Now close the terminal window and open a fresh one (so everything is loaded cleanly)\n"
+echo -e "After you have done that, navigate back to the research-master directory and type: workon bioinformatics
+
