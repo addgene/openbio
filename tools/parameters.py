1698a07e0cbbc1453f8c1f8a746a69cca9d4ba2c
Daniela Bourges <daniela@addgene.org>
2018-05-21 00:00:30 -0400

    Added more help text

--- tools/parameters.py
+++ tools/parameters.py
@@ -1,5 +1,8 @@
 # recombination.py parameters
+
+# The number of bases to retrieve after the seed sequences
 TAIL = 60
+# The number of bases to retrieve before the seed sequence
 HEAD = 60
 
 seed_sequences = {
