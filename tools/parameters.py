27beb4108750761e541b7df721d9d0457ac7f516
Dan BW <dbourgesw@users.noreply.github.com>
2018-11-08 22:01:03 -0500

    Factored common utilities out of recombination.py; separated parameters into classes

--- tools/parameters.py
+++ tools/parameters.py
@@ -1,18 +1,43 @@
-# recombination.py parameters
+########## recombination.py parameters
+class Recombination_Parameters(object):
 
-# The number of bases to retrieve after the seed sequences
-TAIL = 10
-# The number of bases to retrieve before the seed sequence
-HEAD = 10
+    # Change these two values to the folders you prefer - use an absolute path e.g. /Users/Harry/fastq-data and
+    # /Users/Harry/csv-data or a path relative to the tools directory.
+    # You may use the same folder for input and output.
+    input_folder = "/Users/Daniela/Home/Dev/Workspace/research/data/bla"
+    output_folder = "/Users/Daniela/Home/Dev/Workspace/research/data/bla"
 
-seed_sequences = {
-    "loxP": "ATAACTTCGTATAGCATACATTATACGAAGTTAT",
-    "lox2272": "ATAACTTCGTATAGGATACTTTATACGAAGTTAT",
-}
+    # The number of bases to retrieve before the seed sequence
+    HEAD = 10
 
-# Change these two values to the folders you prefer - use an absolute path e.g. /Users/Harry/fastq-data and
-# /Users/Harry/csv-data or a path relative to the tools directory.
-# You may use the same folder for input and output.
+    # The number of bases to retrieve after the seed sequences
+    TAIL = 10
 
-input_folder = "data"
-output_folder = "data"
+    seed_sequences = {
+        "loxP": "ATAACTTCGTATAGCATACATTATACGAAGTTAT",
+        "lox2272": "ATAACTTCGTATAGGATACTTTATACGAAGTTAT",
+    }
+
+
+########## serotype_report.py parameters
+class Serotype_Report_Parameters(object):
+
+    # Change these two values to the folders you prefer - use an absolute path e.g. /Users/Harry/fastq-data and
+    # /Users/Harry/csv-data or a path relative to the tools directory.
+    # You may use the same folder for input and output.
+    input_folder = "/Users/Daniela/Home/Dev/Workspace/research/data/bla"
+    output_folder = "/Users/Daniela/Home/Dev/Workspace/research/data/bla"
+
+    # These are the signatures that will be matched. The first part is the name, the second part is the actual signature
+    signatures = {
+        "AAV1": "AGTGCTTCAACGGGGGCCAG",
+        "AAV2": "AACAGACAAGCAGCTACCGCA",
+        "AAV5": "TCCAAGCCTTCCACCTCGTCAGACGCCGAA",
+        "AAV7": "AGTGAAACTGCAGGTAGTACC",
+        "AAV8": "GCAAAACACGGCTCCTCAAAT",
+        "AAV9": "AGTGCCCAAGCACAGGCGCA",
+        "AAVrh10": "CTACAAATCTACAAATGTGGACTTTG",
+        "PHPeB": "CTTTGGCGGTGCCTTTTAAGGCACAGGCGCAGA",
+        "PHPS": "AGGCGGTTAGGACGTCTTTGGCACAGGCGCAGA",
+        "AAVrg": "TAGCAGACCAAGACTACACAAAAACTGCT",
+    }
---
+++ tools/utils.py
@@ -0,0 +1,49 @@
+import argparse
+import logging
+import os
+import sys
+
+from Bio import SeqIO
+from setuptools import glob
+
+L = logging.getLogger(__name__)
+
+
+def read_fastq_file(file_name):
+    # type: (str) -> List[str]
+    reads = []
+    with open(file_name, "rU") as handle:
+        for record in SeqIO.parse(handle, "fastq"):
+            reads.append(str(record.seq))
+            reads.append(str(record.seq.reverse_complement()))
+    return reads
+
+
+def setup_dirs(params):
+    input = params.input_folder
+    output = params.output_folder
+
+    if not os.path.isdir(input):
+        raise ValueError('The folder ' + input + ' does not exist.')
+
+    if not os.path.isdir(output):
+        L.info('\nThe folder ' + output + ' does not exist. Creating it...\n')
+        os.mkdir(output)
+
+
+def get_fastq_files(params):
+    # type: (object) -> List[str]
+
+    # grab all files ending in .fastq
+    input_files = [input_file for input_file in glob.glob("{}/*.fastq".format(params.input_folder))]
+    if not input_files:
+        raise ValueError('No FASTQ files in folder: ' + params.input_folder)
+
+    return input_files
+
+
+def log_to_stdout(level):
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+    logging.getLogger().addHandler(handler)
+    logging.getLogger().setLevel(level)
