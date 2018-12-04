ffb89a00fa580e4aeca06dd6d29e8a9818600eca
Dan BW <dbourgesw@users.noreply.github.com>
2018-12-04 00:45:42 -0500

    Added params for Recombination

--- toolkit/parameters.py
+++ toolkit/parameters.py
@@ -1,3 +1,24 @@
+########## recombination.py parameters
+class Recombination_Parameters(object):
+
+    # Change these two values to the folders you prefer - use an absolute path e.g. /Users/Harry/fastq-data and
+    # /Users/Harry/csv-data or a path relative to the tools directory.
+    # You may use the same folder for input and output.
+    input_folder = "data"
+    output_folder = "data"
+
+    # The number of bases to retrieve before the seed sequence
+    HEAD = 10
+
+    # The number of bases to retrieve after the seed sequences
+    TAIL = 10
+
+    seed_sequences = {
+        "loxP": "ATAACTTCGTATAGCATACATTATACGAAGTTAT",
+        "lox2272": "ATAACTTCGTATAGGATACTTTATACGAAGTTAT",
+    }
+
+
 ########## serotype_report.py parameters
 class Serotypes_Parameters(object):
 
