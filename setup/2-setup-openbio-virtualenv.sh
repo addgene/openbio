c050da58d2498561357432dc02daa848fb81df2a
Dan BW <dbourgesw@users.noreply.github.com>
2019-09-15 21:48:15 -0400

    Fix set up issues: use python 3, account for no git

--- setup/2-setup-openbio-virtualenv.sh
+++ setup/2-setup-openbio-virtualenv.sh
@@ -15,19 +15,16 @@ if [ "$1" == "nuke" ]; then
     rmvirtualenv $ve_name
 fi
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "$script_dir" || exit 1
-
-git_dir=$(git rev-parse --show-toplevel)
-pushd "$git_dir" || exit 1
-
 if [ ! -e "$WORKON_HOME/$ve_name/bin/activate" ]; then
-    mkvirtualenv $ve_name
+    mkvirtualenv --python=python3 $ve_name
 fi
 
 workon $ve_name
 
-pip3 install -r requirements.txt
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$script_dir" || exit 1
+
+pip3 install -r ../requirements.txt
 
 grep VIRTUALENVWRAPPER_PYTHON $HOME/.bash_profile || echo "export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3" >> $HOME/.bash_profile
 grep "source virtualenvwrapper.sh" $HOME/.bash_profile || echo "source virtualenvwrapper.sh" >> $HOME/.bash_profile
